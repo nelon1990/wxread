61225401346e982f514417a47cff044e365d419b
nelon1990 <470174440@qq.com>
2017-09-28 17:26:38 +0800

    Initial commit

---
+++ index.ios.js
@@ -0,0 +1,11 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React from 'react';
+import App from './main/src/app'
+import {AppRegistry,} from 'react-native';
+
+AppRegistry.registerComponent('wxread', () => App);
